e9b55041c137f1702b404aa9693e5a6004d6c290
chetan <chetan.ccd318@gmail.com>
2026-04-09 21:47:08 +0530

    first commit

---
+++ Chetan.java
@@ -0,0 +1,9 @@
+public class Chetan {
+	public static void main(String[] args) {
+		System.out.println("26th January is celebrated as Republic Day in India.");
+		System.out.println("On this day in 1950, the Constitution of India came into effect.");
+		System.out.println("It is a national holiday and is celebrated with great pride and enthusiasm.");
+		System.out.println("The main celebration takes place at Rajpath, New Delhi, with a grand parade.");
+		System.out.println("Republic Day honors the spirit of independent and democratic India.");
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Republictojavecode
+# Republictojavecode
