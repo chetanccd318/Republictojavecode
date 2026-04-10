0ca342d9f8f8dfd55ad43f2b384e8cc1690efd60
Chetan18 <chetan.ccd318@gmail.com>
2026-04-10 18:14:11 +0530

    Remove unnecessary line breaks in output message

--- Chetan.java
+++ Chetan.java
@@ -1,7 +1,5 @@
 public class Chetan {
 	public static void main(String[] args) {
-		System.out.println("hi chetan , You are doing good , learned about  CI AND CD through Jenkins, 
-						   Today, We are focusing on Pipeline job using  groovy language, Just chec update on jenkins after editing in Github");
-		
+		System.out.println("hi chetan , You are doing good , learned about  CI AND CD through Jenkins, Today, We are focusing on Pipeline job using  groovy language, Just chec update on jenkins after editing in Github");
 	}
 }
